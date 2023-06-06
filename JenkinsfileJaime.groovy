8348bd5afd2637f73483f157f900b458620281eb
Jaime <48511495+jaimemg@users.noreply.github.com>
2023-06-06 11:13:02 +0200

    Add files via upload

--- JenkinsfileJaime.groovy
+++ JenkinsfileJaime.groovy
@@ -25,15 +25,30 @@ pipeline {
                 sh 'make test-api'
                 archiveArtifacts artifacts: 'results/*.xml'
             }
+            post {
+                always {
+                    junit 'results/api_result.xml'
+                }
+            }
         }
         stage('E2E tests') {
             steps {
                 sh 'make test-e2e'
                 archiveArtifacts artifacts: 'results/*.xml'
             }
+            post {
+                always {
+                    junit 'results/e2e_result.xml'
+                }
+            }
         }
     }
     post {
+         failure {
+            emailext body: 'El pipeline ha fallado. Por favor, revisa los registros para obtener más detalles.',
+                     subject: 'Fallo en la ejecución del pipeline',
+                     to: 'jaimeunir@example.com'
+        }
         always {
             junit 'results/*_result.xml'
             cleanWs()
